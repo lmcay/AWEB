5ee36be3ed7a76b1ce75e99434fa54e4430b93f7
sjh-706 <sjh-706@sjh-706.local>
2024-11-27 11:34:52 +0800

    First Activity: ADET 1

---
+++ prelim_lab_1/src/app/comp3/comp3.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp3',
+  imports: [],
+  templateUrl: './comp3.component.html',
+  styleUrl: './comp3.component.css'
+})
+export class Comp3Component {
+
+}
---
+++ prelim_lab_1/src/app/comp4/comp4.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-comp4',
+  imports: [],
+  templateUrl: './comp4.component.html',
+  styleUrl: './comp4.component.css'
+})
+export class Comp4Component {
+
+}
